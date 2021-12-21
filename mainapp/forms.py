e335a78df64c1b35bfde19dfc431da77484ab223
Juan Manuel Echeverria <factorzero@gmail.com>
2021-12-21 09:34:05 -0300

    is_staff added

--- mainapp/forms.py
+++ mainapp/forms.py
@@ -11,6 +11,6 @@ class RegisterForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
+        fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2', 'is_staff']
         
 
