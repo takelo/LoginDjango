b6687bbd26b817083b654b6008cfb121bced207d
Juan Manuel Echeverria <factorzero@gmail.com>
2021-12-21 13:37:37 -0300

    superuser and staff checkbox in form

--- mainapp/forms.py
+++ mainapp/forms.py
@@ -8,9 +8,11 @@ class RegisterForm(UserCreationForm):
         super(RegisterForm, self).__init__(*args, **kwargs)
         # Making location required
         self.fields['email'].required = True
+        self.fields['is_superuser'].required = True
+        self.fields['is_staff'].required = True
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2', 'is_staff']
+        fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2', 'is_staff', 'is_superuser']
         
 
